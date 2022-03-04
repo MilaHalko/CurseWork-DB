56b688feca9f3606281ca7eb58efc0e9b8a7e340
Mila Halko <milagalko1@gmail.com>
2022-03-04 19:41:14 +0200

    Curse work is finished

--- script/Indexes.sql
+++ script/Indexes.sql
@@ -1,3 +1,13 @@
 use master
 use LandCompany
 go
+
+select n.ID as NaturalID, n.Surname
+from Natural n
+where Surname in ('Wemyss', 'Heddy', 'Trench', 'Coyte', 'Soar', 'Sagrott')
+go
+
+create index NaturalID on Natural(Surname) 
+go
+
+--02471891
